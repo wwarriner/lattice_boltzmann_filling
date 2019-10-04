9d732a94579e97dc60baef30765d35e086827bac
UAB\wwarr <wwarr@uab.edu>
2019-10-03 20:27:51 -0500

    correct implementation of cube segment volume

--- cube_segment_volume.m
+++ cube_segment_volume.m
@@ -4,69 +4,90 @@
 % normal must be unit normal
 % offset is distance from center to plane, negative means plane is on negative
 % side of origin, positive means plane on positive side
-function volume = cube_segment_volume( normal, offset )
+function volume = cube_segment_volume( normals, offsets )
 
-% anti-symmetry of offset about origin
-flip = false;
-if 0 < offset
-    offset = -offset;
-    flip = true;
+% setup output
+volume = nan( size( offsets ) );
+
+% exploit antisymmetry of offset about origin
+flip = 0 < offsets;
+offsets( flip ) = -offsets( flip );
+
+% exploit remaining cube symmetries
+normals = sort( abs( normals ), 2 );
+
+% case where plane is outside cube
+origin_dist = normals * [ -0.5 -0.5 -0.5 ].' - offsets;
+plane_outside = 0 <= origin_dist;
+consider = plane_outside;
+volume( consider ) = 0;
+done = consider;
+
+% case where plane does not intercept bottom xy-face inside cube
+n = inf( size( offsets ) );
+n( ~done ) = normals( ~done, : ) * [ 0.5 0.5 -0.5 ].' - offsets( ~done );
+four_verts_inside = n <= 0;
+consider = ~done & four_verts_inside;
+zi = [ -0.5 -0.5; -0.5 0.5; 0.5 -0.5; 0.5 0.5 ];
+z = compute_intercept( normals( consider, [ 1 2 ] ), zi, normals( consider, 3 ), offsets( consider ) );
+z = z + 0.5;
+volume( consider ) = mean( z, 2 );
+done = consider | done;
+
+% case where unit normal has x == 0
+flat = normals( :, 1 ) == 0;
+consider = ~done & flat;
+zi = [ -0.5 -0.5 ];
+z = compute_intercept( normals( consider, [ 1 2 ] ), zi, normals( consider, 3 ), offsets( consider ) );
+z = max( min( z + 0.5, 1 ), 0 );
+yi = [ -0.5 -0.5 ];
+y = compute_intercept( normals( consider, [ 1 3 ] ), yi, normals( consider, 2 ), offsets( consider ) );
+y = max( min( y + 0.5, 1 ), 0 );
+volume( consider ) = y .* z / 2;
+done = consider | done;
+
+% remaining case
+consider = ~done;
+zi = [ -0.5 -0.5; -0.5 0.5; 0.5 -0.5 ];
+z = compute_intercept( normals( consider, [ 1 2 ] ), zi, normals( consider, 3 ), offsets( consider ) );
+z = z + 0.5;
+zc = max( min( z, 1 ), 0 );
+yi = [ -0.5 -0.5; -0.5 0.5; 0.5 -0.5 ];
+y = compute_intercept( normals( consider, [ 1 3 ] ), yi, normals( consider, 2 ), offsets( consider ) );
+y = y + 0.5;
+yc = max( min( y, 1 ), 0 );
+xi = [ -0.5 -0.5; -0.5 0.5; 0.5 -0.5 ];
+x = compute_intercept( normals( consider, [ 2 3 ] ), xi, normals( consider, 1 ), offsets( consider ) );
+x = x + 0.5;
+xc = max( min( x, 1 ), 0 );
+V100 = yc( :, 3 ) .* zc( :, 3 ) .* ( x( :, 1 ) - xc( :, 1 ) );
+V010 = xc( :, 3 ) .* zc( :, 2 ) .* ( y( :, 1 ) - yc( :, 1 ) );
+V001 = xc( :, 2 ) .* yc( :, 2 ) .* ( z( :, 1 ) - zc( :, 1 ) );
+V000 = x( :, 1 ) .* y( :, 1 ) .* z( :, 1 );
+volume( consider ) = ( V000 - ( V100 + V010 + V001 ) ) / 6;
+done = consider | done;
+cases_done = sum( done );
+
+% exploit antisymmetry of offset about origin
+volume( flip ) = 1 - volume( flip );
+
+% sanity check
+assert( ~any( isnan( volume ) ) );
+assert( sum( cases_done ) == numel( offsets ) );
+assert( all( 0.0 <= volume ) );
+assert( all( volume <= 1.0 ) );
+
 end
 
-normal = sort( abs( normal ) ); % take advantage of remaining cube symmetries
 
-origin_dist = dot( normal, [ -0.5 -0.5 -0.5 ] ) - offset;
-if 0 <= origin_dist
-    volume = 0;
-else
-    n = dot( normal, [ 0.5 0.5 -0.5 ] ) - offset;
-    if n <= 0
-        zi = [ -0.5 -0.5; -0.5 0.5; 0.5 -0.5; 0.5 0.5 ];
-        z = compute_intercept( normal( [ 1 2 ] ).', zi, normal( 3 ), offset );
-        z = z + 0.5;
-        volume = mean( z );
-        volume = max( min( volume, 1 ), 0 );
-    elseif normal( 1 ) == 0
-        zi = [ -0.5 -0.5 ];
-        z = compute_intercept( normal( [ 1 2 ] ).', zi, normal( 3 ), offset );
-        z = max( min( z + 0.5, 1 ), 0 );
-        yi = [ -0.5 -0.5 ];
-        y = compute_intercept( normal( [ 1 3 ] ).', yi, normal( 2 ), offset );
-        y = max( min( y + 0.5, 1 ), 0 );
-        volume = y( 1 ) * z( 1 ) / 2;
-    else
+function p = compute_intercept( normals, coords, coeff, offset )
 
-        zi = [ -0.5 -0.5; -0.5 0.5; 0.5 -0.5 ];
-        z = compute_intercept( normal( [ 1 2 ] ).', zi, normal( 3 ), offset );
-        z = z + 0.5;
-        zc = max( min( z, 1 ), 0 );
-        yi = [ -0.5 -0.5; -0.5 0.5; 0.5 -0.5 ];
-        y = compute_intercept( normal( [ 1 3 ] ).', yi, normal( 2 ), offset );
-        y = y + 0.5;
-        yc = max( min( y, 1 ), 0 );
-        xi = [ -0.5 -0.5; -0.5 0.5; 0.5 -0.5 ];
-        x = compute_intercept( normal( [ 2 3 ] ).', xi, normal( 1 ), offset );
-        x = x + 0.5;
-        xc = max( min( x, 1 ), 0 );
-        V100 = yc( 3 ) * zc( 3 ) * ( x( 1 ) - xc( 1 ) );
-        V010 = xc( 3 ) * zc( 2 ) * ( y( 1 ) - yc( 1 ) );
-        V001 = xc( 2 ) * yc( 2 ) * ( z( 1 ) - zc( 1 ) );
-        V000 = x( 1 ) * y( 1 ) * z( 1 );
-        volume = ( V000 - ( V100 + V010 + V001 ) ) / 6;
-    end
+if isempty( normals )
+    p = [];
+    return
 end
 
-% anti-symmetry of offset about origin
-if flip
-    volume = 1 - volume;
-end
-
-end
-
-
-function p = compute_intercept( normal, coords, coeff, offset )
-
-p = -( coords * normal - offset ) ./ coeff;
+p = -( normals * coords.' - offset ) ./ coeff;
 
 end
 
--- cube_segment_volume_test.m
+++ cube_segment_volume_test.m
@@ -1,20 +1,37 @@
 function cube_segment_volume_test()
 
-n = sort( rand( [ 100 3 ] ), 1 );
-n = n ./ vecnorm( n, 2, 2 );
+rng( 314159 );
 
-off = sort( rescale( rand( [ 100 1 ] ), -sqrt( 3 ) / 2, 0 ) );
-off = [ off; 0.0; flip( -off ) ];
+n = 2 * rand( [ 1000 3 ] ) - 1;
+n = [ 
+    n;
+    0 0 1;
+    0 1 0;
+    1 0 0;
+    1 1 0;
+    1 0 1;
+    0 1 1;
+    1 1 1;
+    0.0480264745524219 0.684013838616227 0.727886341624542
+    ];
+n = n ./ vecnorm( n, 2, 2 );
+n = sortrows( n );
+
+off = rescale( rand( [ 1000 1 ] ), -sqrt( 3 ) / 2, 0 );
+off = [ -sqrt( 3 ) / 2; -sqrt( 2 ) / 2; off ];
+off = [ off; 0.0; -off ];
+off = sort( off );
 
 [ n_index, off ] = meshgrid( 1 : size( n, 1 ), off );
 
-v = nan( size( n_index ) );
-tic;
-for i = 1 : numel( n_index )
-    v( i ) = cube_segment_volume( n( n_index( i ), : ), off( i ) );
-end
-toc;
+fprintf( 1, "%i elements calculated in:\n", numel( n_index ) );
+t = tic;
+v = cube_segment_volume( n( n_index( : ), : ), off( : ) );
+v = reshape( v, size( n_index ) );
+toc( t );
 
+assert( all( 0.0 <= v, "all" ) );
+assert( all( v <= 1.0, "all" ) );
 assert( all( abs( v + flip( v, 1 ) - 1 ) <= 1e-5, "all" ) );
 
 end
